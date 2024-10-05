e6937e44af0c28c45960e1b78c5ca8c1a60274b0
jsyoo_React <112743397+jsyoo1229@users.noreply.github.com>
2024-10-05 14:41:58 +0000

    post 모델 초기 설정

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class post(models.model):
+    title = models.CharField(max_length=50);
+    context = models.TextField();
+    write_date = models.DateField(auto_now_add=True);
+    update_date = models.DateTimeField(auto_now=True);
+    
+    
+    
+
+
